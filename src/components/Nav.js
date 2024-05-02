6dbb122f0a429dc776a2b4b774730084e1ff5ce2
Genaro Arredondo <garred97@gmail.com>
2024-05-01 18:21:18 -0600

    Added semantic structure and meta tags

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from '../assets/Logo .svg'
+
+const Nav = () => {
+    return (
+        <nav>
+            <a href="/">
+                <img src={logo} alt="logo" />
+            </a>
+
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Services</a></li>
+                <li><a href="/">Menu</a></li>
+                <li><a href="/">Reservations</a></li>
+                <li><a href="/">Order Online</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
